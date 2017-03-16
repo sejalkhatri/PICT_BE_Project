666c014b38dc8a968d00deeb254b55ab9383e29d
Sejal <sejal.khatri5@gmail.com>
2017-03-17 03:15:31 +0530

    Add database seeding functionality

--- implementation/db/seeds.rb
+++ implementation/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+Vendingmachine.destroy_all
+Vendingmachine.create!(id: 1,
+                        place: 'Katraj Dairy')
+
+
+10.times do |index|
+  Product.create!(id: index,
+                  name: "Type#{index}",
+                  price: index,
+                  stock: index+1,
+                  vendingmachine_id: 1)
+end
+
+p "Created #{Product.count} products"
